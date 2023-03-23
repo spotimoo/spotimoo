541231462b344c5f5fb7f61d48c80aee298a465a
Egor <bronnikov.40@mail.ru>
2023-03-23 23:22:16 +0300

    Update documentation string

--- spotimoo/exceptions.py
+++ spotimoo/exceptions.py
@@ -1,5 +1,5 @@
 class IncorrectRequest(Exception):
-    """Raises when we got request errror."""
+    """Raises when we got request error."""
 
     def __init__(self, status_code: int) -> None:
         super().__init__(f"Incorrect request: {status_code}")
